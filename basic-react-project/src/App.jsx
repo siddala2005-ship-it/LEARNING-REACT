1cb79d00f6bc7e199f30bf0e86a04c4dade1b1f9
siddala sai kumar <siddala2005@gmail.com>
2026-03-06 16:11:14 +0530

    react basics

---
+++ basic-react-project/src/App.jsx
@@ -0,0 +1,25 @@
+
+import './App.css'
+
+
+function Title(){
+return <h1>this is title!</h1>;
+
+}
+
+function App() {
+
+  return (
+    <div>
+      <h1>this is app component</h1>
+      <p>inside app component :</p>
+      <Title/>
+    </div>
+  )
+    
+  
+  
+    
+}
+
+export default App
