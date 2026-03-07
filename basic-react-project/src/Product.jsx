20f5f0962e3484b7e96beec092467e5144d381d5
siddala sai kumar <siddala2005@gmail.com>
2026-03-07 16:16:16 +0530

    product css added

--- basic-react-project/src/Product.jsx
+++ basic-react-project/src/Product.jsx
@@ -1,9 +1,10 @@
+import './Product.css'
 function Product(){
     return(
-        <>
+        <div className="Product">
     <h2>product</h2>
     <h5>description</h5>
-    </>
+    </div>
     )
 }
 export default Product
